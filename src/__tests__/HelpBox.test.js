9df186c77944d6ae2a541a08f44c69783c0a0bb3
Rishad Omar <rishad@aux.co.za>
2022-07-21 13:09:17 +0200

    Renamed to js to prevent error in generated test file.

---
+++ src/__tests__/HelpBox.test.js
@@ -0,0 +1,5 @@
+describe('my test', () => {
+    it('some text', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
--- src/__tests__/HelpBox.test.ts
+++
@@ -1,9 +0,0 @@
-//import { HelpBox } from '../index';
-
-test('My HelpBox', () => {
-    expect(2 + 2).toBe(4);
-
-    // expect(HelpBox('Test Help box', { url: '/#', title: 'Terms and Conditions', pdf: 'TermsAndConditions.pdf' })).toBe(
-    //     '',
-    // );
-});
